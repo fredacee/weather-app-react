0af26464344948eb4ab93ab846d3aa445e5ca5cd
fredacee <111201560+fredacee@users.noreply.github.com>
2023-06-14 12:14:56 +0100

    made some corrections and installed bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -11,10 +11,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city.."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="sudmit" value="search" />
+            <input
+              type="sudmit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -27,14 +32,12 @@ export default function Weather() {
         <div className="col-6">
           <div className="clearfix">
             <img
-              src="https://ssl.gstatic.com/onebox/weather/48/cloudy.png"
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
               alt="cloudy-icon"
               className="float-left"
             />
-            <div className="float-left">
-              <span className="temperature">28</span>
-              <span className="units">℃</span>
-            </div>
+            <span className="temperature">28</span>
+            <span className="units">℃ | ℉ </span>
           </div>
         </div>
         <div className="col-6">
